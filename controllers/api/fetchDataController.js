435077fc904213198690a695fb8a7819ded37da0
qui-per32 <qui.per.gonz32@gmail.com>
2018-07-27 00:17:34 +0200

    performance and volatility calculated on api

--- controllers/api/fetchDataController.js
+++ controllers/api/fetchDataController.js
@@ -5,12 +5,17 @@ let CalcService = require('../../service/calcService');
 class fetchDataController extends Controller {
     constructor(req, res, next) {
         super(req, res, next);
+        let isin = this.req.query.isin;
+        let dateFrom = this.req.query.dateFrom;
+        let dateTo = this.req.query.dateTo;
 
     }
     fetchData() {
         let isin = this.req.query.isin;
         let dateFrom = this.req.query.dateFrom;
         let dateTo = this.req.query.dateTo;
+        let precio = [];
+
         return new Promise((resolve, reject) => {
             CuerpoModel.find({
                 isin: isin,
@@ -20,25 +25,27 @@ class fetchDataController extends Controller {
                 },
             }, (err, data) => {
                 if (err) reject(err);
-                  let calcService = new CalcService();
-             
-                  
-                calcService.resolveDataCalc(data)
-                  
-
+                let calcService = new CalcService();
+                // console.log(precio);
                 
+                
+
+                calcService.resolveDataCalc(isin, dateFrom, dateTo, data)
+
+
+
             })
         })
     }
 
-// let calcService = new CalcService();
-// calcService.performance((isin,dateTo,dateFrom)=>{
+    // let calcService = new CalcService();
+    // calcService.performance((isin,dateTo,dateFrom)=>{
 
-// })
+    // })
 
 
 
-// this.res.json(dateTo);
+    // this.res.json(dateTo);
 
 }
 module.exports = fetchDataController;
--- service/calcService.js
+++ service/calcService.js
@@ -6,25 +6,70 @@ class calcService {
 
 
 
-    resolveDataCalc(data) {
-        
+    resolveDataCalc(isin, dateFrom, dateTo, data) {
+        // let isin = this.req.query.isin;
+        // let dateFrom = this.req.query.dateFrom;
+        // let dateTo = this.req.query.dateTo;
+        // console.log(data);
+
 
         let precioFin = 0;
         let precioInicio = 0;
+        let precio = [];
+        let varianza = [];
 
-        if (data[0].precio){
+        if (data[0].precio) {
             precioInicio = data[0].precio;
 
         }
-        if (data[data.length - 1].precio){
+        if (data[data.length - 1].precio) {
             precioFin = data[data.length - 1].precio;
         }
         // data = JSON.parse(JSON.stringify(data))
         // console.log(data[0].precio,data[0],data[data.length-1]);
-        this.result = precioFin - precioInicio 
+        this.result = precioFin - precioInicio
         this.total = this.result / precioInicio;
-        console.log(this.total);
+        console.log("performance->" + this.total);
+
+        let media = 0;
+        // console.log(data.length);
+
+        for (let i = 0; i < data.length; i++) {
+            precio.push(data[i].precio)
+
+        }
+
+        for (let i = 0; i < precio.length; i++) {
+            media += precio[i];
+
+        }
+
+        media = media / precio.length;
+        // console.log(media);
+
+
+        for (let i = 0; i < precio.length; i++) {
+            varianza.push(Math.pow(precio[i] - media, 2))
+        }
+
+        let varianzaTotal = 0;
+
+        for (let i = 0; i < varianza.length; i++) {
+            varianzaTotal += varianza[i]
+        }
+
+        varianzaTotal = varianzaTotal / (precio.length - 1);
+
+        let volatility = Math.sqrt(varianzaTotal)
+
+        console.log("volavility->"+ volatility);
         
+        // console.log(isin);
+
+        // console.log(data[0].precio);
+
+
+
 
     }
 
